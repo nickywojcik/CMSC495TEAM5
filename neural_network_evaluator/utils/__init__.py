02c47df0255a403d9fec6865c5bd03fc1670d242
jackboswell <jboswell19@student.umgc.edu>
2023-11-18 14:27:06 -0700

    Removed ImageNetCategories

--- neural_network_evaluator/utils/__init__.py
+++ neural_network_evaluator/utils/__init__.py
@@ -4,5 +4,4 @@ Helper file for util loading
 Paul Wojcik, Jack Boswell, Andrew Rios, Nelson Romero, Nikhil Thomas
 """
 from .analysis_results import AnalysisResults
-from .imagenet_categories import ImageNetCategories
 from .web_image import WebImage
