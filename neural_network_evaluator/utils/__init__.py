7be940308a402d49628a33ef292c293d871859a7
jackboswell <jboswell19@student.umgc.edu>
2023-11-18 13:34:47 -0700

    Initial __init__.py for utils

---
+++ neural_network_evaluator/utils/__init__.py
@@ -0,0 +1,8 @@
+"""  
+CMSC Team 5
+Helper file for util loading
+Paul Wojcik, Jack Boswell, Andrew Rios, Nelson Romero, Nikhil Thomas
+"""
+from .analysis_results import AnalysisResults
+from .imagenet_categories import ImageNetCategories
+from .web_image import WebImage
