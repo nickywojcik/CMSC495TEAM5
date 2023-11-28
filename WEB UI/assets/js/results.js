8e5804845219d79a2cf0128a040c80d025bdd0d2
jackboswell <jboswell19@student.umgc.edu>
2023-11-27 18:16:37 -0700

    No longer needed

--- WEB UI/assets/js/results.js
+++
@@ -1,7 +0,0 @@
-function results() {
-    // Placeholder for future code to handle image processing
-    console.log("Processing image...");
-
-    // Placeholder for navigation - replace with actual results page
-    location.href = 'results_page.html';
-}
